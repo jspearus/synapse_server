992800d9d1c9c23dd54ad063723961c36a6ab18e
jspearus <jsperaus@gmail.com>
2023-03-07 20:08:57 -0600

    started new ui

--- redthumb/src/App.js
+++ redthumb/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-import useWebSocket from 'react-use-websocket';
+import { Card, CardContent, Box, CssBaseline, Typography } from '@mui/material';
+import { Colors } from "./styles/colors";
 
 import './App.css';
 
@@ -13,23 +13,33 @@ const chatSocket = new WebSocket(WS_URL);
 function sendMsg(msg, dest) {
   chatSocket.send(JSON.stringify({
     'message': msg,
-    'username': 'web',
+    'username': 'red',
     'destination': dest,
   }));
 
 }
 
 function sendClicked() {
-  sendMsg('clicked', 'red')
+  sendMsg('status', 'plants')
 }
 
 chatSocket.onmessage = function (event) {
   const json = JSON.parse(event.data);
   try {
-    if ((json.destination === "red")) {
+    if (json.destination === "red" || json.destination === "web") {
 
-      console.log(`[message] Data received from server: ${json.username}`);
+      // console.log(`[message] Data received from server: ${json.username}`);
       console.log(json.message);
+      if (json.message.includes('mlevels')) {
+        const lvls = json.message.split(",");
+        let label = document.getElementById("status");
+        label.innerHTML = "plant1 Level- " + lvls[1] + " plant2 Level- " + lvls[2] + " plant3 Level- " + lvls[3];
+      }
+      else if (json.message.includes('tanklvl')) {
+        const lvl = json.message.split(":");
+        let label = document.getElementById("tanklvl");
+        label.innerHTML = "Tank Level - " + lvl[1];
+      }
     }
   } catch (err) {
     console.log(err);
@@ -40,14 +50,22 @@ chatSocket.onmessage = function (event) {
 
 function App() {
   chatSocket.onopen = (event) => {
-    sendMsg('hello', 'red')
+    sendMsg('status', 'plants')
   };
 
   return (
-    <>
-      <h1>Red Thumb Dashboard</h1>
-      <button onClick={sendClicked}>click</button>
-    </>
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <CssBaseline />
+      <Card sx={{ my: 10, mx: 10, width: "75%" }}>
+        <CardContent>
+          <Typography
+            variant='h4'
+            sx={{ color: Colors.primary }}>Red Thumb</Typography>
+          <Typography id='status'> </Typography>
+          <Typography id='tanklvl'> </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
---
+++ redthumb/src/components/primaryAppbar.js
@@ -0,0 +1,9 @@
+import { useTheme } from "@mui/material/styles";
+
+export default function PrimaryAppbar() {
+
+    const theme = useTheme()
+    return (
+        <h1>appbar</h1>
+    )
+}
